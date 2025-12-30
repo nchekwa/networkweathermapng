7c4901ae2d45639a943dd19452d61ae0f3f9e96a
Artur Zdolinski <artur@zdolinski.com>
2025-12-30 17:28:30 +0100

    remove temporary visual editor backup file + update docs/env

--- src/app/Views/editor/visual_temp.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Copy the current visual.php to temp file for repair
-$content = file_get_contents('/mnt/d/Projects/zabbix-weathermap/src/app/Views/editor/visual.php');
-file_put_contents('/mnt/d/Projects/zabbix-weathermap/src/app/Views/editor/visual_temp.php', $content);
